3a9a7b39d12558472badaf7b3a0d3a3162fc4774
Julian Rodriguez <julian.rodriguez@sofka.com.cogit config --global user.email julian.rodriguez@sofka.com.co>
2025-12-22 14:35:13 -0500

    Feat test

---
+++ Backend-SistemaPedidos/admin-service/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        types: ['jest', 'node'],
+        esModuleInterop: true,
+        skipLibCheck: true,
+      },
+    }],
+  },
+};
